71209aacadf32ed1c9f875b9ecb8e74556fd572c
Isotirov <vanko_215@abv.bg>
2022-04-13 23:32:41 +0300

    Added reset password link

--- cook_book/cook_book_auth/urls.py
+++ cook_book/cook_book_auth/urls.py
@@ -1,10 +1,25 @@
+from django.contrib.auth.views import PasswordResetConfirmView, PasswordResetCompleteView
 from django.urls import path
 
-from cook_book.cook_book_auth.views import LogOut, login_user, activate, SignUp
+from cook_book.cook_book_auth.views import LogOut, remember_me, activate, SignUp, ShowTemplateEmailSend, \
+    ShowTemplateActivationSuccess, PasswordResetViewCustom, PasswordChangeViewCustom, ResetPasswordSent, NoSuchEmail
 
 urlpatterns = [
     path('sign-up/', SignUp.as_view(), name='sign up'),
     path('activate/<str:uidb64>/<str:token>/', activate, name='activate'),
-    path('sign-in/', login_user, name='sign in'),
+    path('sign-in/', remember_me, name='sign in'),
     path('log-out/', LogOut.as_view(), name='log out'),
+
+    path('mail-sent/', ShowTemplateEmailSend.as_view(), name='mail sent'),
+    path('success-mail/', ShowTemplateActivationSuccess.as_view(), name='success activation'),
+
+    path('change-password/', PasswordChangeViewCustom.as_view(), name='password change'),
+    path('password-reset/', PasswordResetViewCustom.as_view(), name='password reset mail'),
+    path('reset-password-sent/', ResetPasswordSent.as_view(), name='reset password sent'),
+    path('invalid-email/', NoSuchEmail.as_view(), name='not registered'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='reset_password.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(template_name='success_activation.html'), name='password_reset_complete'),
 ]
+
+
+
--- cook_book/cook_book_auth/views.py
+++ cook_book/cook_book_auth/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import get_user_model, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.sites.shortcuts import get_current_site
+from django.core.exceptions import ValidationError
 from django.core.mail import EmailMessage
 from django.http import HttpResponse
 from django.shortcuts import redirect
@@ -10,10 +12,11 @@ from django.urls import reverse_lazy
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.views import generic as views
+from django.contrib.auth import views as pass_views
 
 from cook_book.cook_book_auth.forms import SignInForm, SignUpForm
 from cook_book.cook_book_auth.helper import calculate_session_expiry_duration
-from cook_book.cook_book_auth.tokens import account_activation_token
+from cook_book.cook_book_auth.tokens import account_activation_token, TokenGenerator
 
 UserModel = get_user_model()
 
@@ -24,23 +27,34 @@ class SignUp(views.CreateView):
     template_name = 'sign_up.html'
     success_url = reverse_lazy('sign in')
 
+    def send_activation_email(self, request, user):
+        current_site = get_current_site(self.request)
+        mail_subject = 'Добре дошли в домашния кулинарник.'
+        message = render_to_string('acc_active_email.html', {
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            'token': account_activation_token.make_token(user),
+        })
+        to_email = user.email
+        email = EmailMessage(
+            mail_subject, message, to=[to_email]
+        )
+        email.send()
+
     def form_valid(self, form):
         if form.is_valid():
             user = form.save()
-            current_site = get_current_site(self.request)
-            mail_subject = 'Добре дошли в домашния кулинарник.'
-            message = render_to_string('acc_active_email.html', {
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': account_activation_token.make_token(user),
-            })
-            to_email = form.cleaned_data.get('email')
-            email = EmailMessage(
-                mail_subject, message, to=[to_email]
-            )
-            email.send()
-            return HttpResponse('Изпратихме линк на пощата ви за потвърждение.')
+            self.send_activation_email(self, user)
+            return redirect('mail sent')
         return super().form_valid(form)
+
+    def post(self, request, *args, **kwargs):
+        if UserModel.objects.filter(email=request.POST['email']).exists():
+            user = UserModel.objects.get(email=request.POST['email'])
+            if not user.is_active:
+                self.send_activation_email(request, user)
+                return redirect('mail sent')
+        return super().post(request, *args, **kwargs)
 
 
 def activate(request, uidb64, token):
@@ -52,13 +66,12 @@ def activate(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-        # login_user(request)
-        return redirect('sign in')
+        return redirect('success activation')
     else:
         return HttpResponse('Линка е невалиден!')
 
 
-def login_user(request):
+def remember_me(request):
     period = calculate_session_expiry_duration()
     response = SignIn.as_view()(request)
     if 'remember' in request.POST:
@@ -82,3 +95,35 @@ class LogOut(LoginRequiredMixin, views.RedirectView):
     def get(self, request, *args, **kwargs):
         logout(request)
         return super().get(request, *args, **kwargs)
+
+
+class ShowTemplateEmailSend(views.TemplateView):
+    template_name = 'mail_sent.html'
+
+
+class ShowTemplateActivationSuccess(views.TemplateView):
+    template_name = 'success_activation.html'
+
+
+class PasswordChangeViewCustom(pass_views.PasswordChangeView):
+    pass
+
+
+class PasswordResetViewCustom(pass_views.PasswordResetView):
+    template_name = 'reset_password.html'
+    email_template_name = 'password_reset_email.html'
+    title = 'Смяна на парола'
+    success_url = reverse_lazy('reset password sent')
+
+    def form_valid(self, form):
+        if not UserModel.objects.filter(email=self.request.POST['email']).exists():
+            return redirect('not registered')
+        return super().form_valid(form)
+
+
+class ResetPasswordSent(views.TemplateView):
+    template_name = 'reset_mail_sent.html'
+
+
+class NoSuchEmail(views.TemplateView):
+    template_name = 'user_does_not_exists.html'
